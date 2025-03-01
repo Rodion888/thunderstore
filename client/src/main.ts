b161dc8f3340b633ac15bcb647fc91818eb02058
Rodion <rodion@Rodions-MacBook-Pro.local>
2025-03-01 19:10:32 +0300

    Fix JSON import for products

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,14 +1,5 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { appConfig } from './app/app.config';
-
-fetch('/assets/env.js')
-  .then(response => response.text())
-  .then(text => {
-    eval(text);
-    bootstrapApplication(AppComponent, appConfig).catch(err => console.error('error to run app', err));
-  })
-  .catch(err => {
-    console.error('error to run env.js:', err);
-    bootstrapApplication(AppComponent, appConfig).catch(err => console.error('error to run app', err));
-  });
+  
+bootstrapApplication(AppComponent, appConfig).catch((err) => console.error(err));
