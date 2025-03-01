7bb674d8a65e300d7f9f254e217e7def7586af27
Rodion <rodion@Rodions-MacBook-Pro.local>
2025-03-01 17:24:14 +0300

    fix

--- client/src/main.ts
+++ client/src/main.ts
@@ -2,4 +2,13 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { appConfig } from './app/app.config';
 
-bootstrapApplication(AppComponent, appConfig).catch((err) => console.error(err));
+fetch('/assets/env.js')
+  .then(response => response.text())
+  .then(text => {
+    eval(text);
+    bootstrapApplication(AppComponent, appConfig).catch(err => console.error('error to run app', err));
+  })
+  .catch(err => {
+    console.error('error to run env.js:', err);
+    bootstrapApplication(AppComponent, appConfig).catch(err => console.error('error to run app', err));
+  });
