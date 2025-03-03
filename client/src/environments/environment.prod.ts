f473016fe114ce674ce4f268846915650d28b8ca
Rodion <rodion@Rodions-MacBook-Pro.local>
2025-03-03 23:46:15 +0300

    fix

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,5 +1,11 @@
+// export const environment = {
+//   production: true,
+//   apiUrl: 'https://thunder-store.ru',
+//   wsUrl: 'wss://thunder-store.ru'
+// };
+
 export const environment = {
   production: true,
-  apiUrl: 'https://thunder-store.ru',
-  wsUrl: 'wss://thunder-store.ru'
+  apiUrl: 'https://thunder-store.ru/api',
+  wsUrl: 'wss://thunder-store.ru/ws/'
 };
--- server/routes/products.js
+++ server/routes/products.js
@@ -1,35 +1,3 @@
-// import { loadProducts, getProductById } from '../services/productService.js';
-
-// export default async function productRoutes(fastify) {
-//   fastify.get('/products', async (req) => {
-//     const { _limit = 10, _page = 1 } = req.query;
-
-//     const products = await loadProducts();
-//     const limit = parseInt(_limit, 10);
-//     const page = parseInt(_page, 10);
-
-//     const start = (page - 1) * limit;
-//     const paginatedProducts = products.slice(start, start + limit);
-
-//     return {
-//       total: products.length,
-//       products: paginatedProducts,
-//       page,
-//       limit,
-//     };
-//   });
-
-//   fastify.get('/products/:id', async (req, reply) => {
-//     const product = await getProductById(req.params.id);
-
-//     if (!product) {
-//       return reply.status(404).send({ message: 'Fail to find product' });
-//     }
-
-//     return reply.send(product);
-//   });
-// }
-
 import { loadProducts, getProductById } from '../services/productService.js';
 
 export default async function productRoutes(fastify) {
